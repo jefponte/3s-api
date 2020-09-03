e4114cab66a397514eeee76e4251e8a0cbd2c0d3
Jefferson Uchoa Ponte <jefponte@gmail.com>
2020-09-03 09:47:09 -0300

    Utilização de selectize default.

--- src/index.php
+++ src/index.php
@@ -64,7 +64,7 @@ spl_autoload_register('autoload');
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <link rel="stylesheet" type="text/css" href="css/list.css" />
-<link rel="stylesheet" type="text/css" href="css/selectize.bootstrap3.css" />
+<link rel="stylesheet" type="text/css" href="css/selectize.default.css" />
 <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 .btn-circle {
@@ -210,12 +210,7 @@ Principal::main();
 <script src="js/selectize.js"></script>
 <script src="js/painel3s.js"></script>
 <script src="js/ocorrencia.js"></script>
-<script>
-$('#select-servicos').selectize({
-    create: false,
-    sortField: 'text'
-});
-</script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="js/barra_2.0.js"></script>
