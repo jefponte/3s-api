a15618a98aa33baa2cc07b5f4984f74e858c1ac7
Jefferson Uchoa Ponte <jefponte@gmail.com>
2020-09-03 10:01:52 -0300

    Área de login.

--- src/classes/custom/controller/Principal.php
+++ src/classes/custom/controller/Principal.php
@@ -5,25 +5,40 @@ class Principal{
     
     public static function main()
     {
-        if(isset($_GET['pagina'])){
-            switch ($_GET['pagina']){
-                case 'painel':
-                    $controller = new OcorrenciaCustomController();
-                    $controller->telaInicialPainel();
-                    break;
-                case 'ocorrencia':
-                    $controller = new OcorrenciaCustomController();
-                    $controller->main();
-                    break;
-                default:
-                    echo '<p>Página solicitada não encontrada.</p>';
-                    break;
-            }
-        }else{
-            $controller = new OcorrenciaCustomController();
-            $controller->main();
+        
+        echo '
             
-        }
+<div class="card mb-4">
+    <div class="card-body">
+';
+
+        
+        $usuarioView = new UsuarioCustomView();
+        $usuarioView->formLogin();
+        echo '<br><br><br><br>
+    </div>
+</div>';
+        
+        
+//         if(isset($_GET['pagina'])){
+//             switch ($_GET['pagina']){
+//                 case 'painel':
+//                     $controller = new OcorrenciaCustomController();
+//                     $controller->telaInicialPainel();
+//                     break;
+//                 case 'ocorrencia':
+//                     $controller = new OcorrenciaCustomController();
+//                     $controller->main();
+//                     break;
+//                 default:
+//                     echo '<p>Página solicitada não encontrada.</p>';
+//                     break;
+//             }
+//         }else{
+//             $controller = new OcorrenciaCustomController();
+//             $controller->main();
+            
+//         }
         
         
     }
