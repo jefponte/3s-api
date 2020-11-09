69f20fd48715c633a29e2e4c0714161b554feef4
Jefferson Uchoa Ponte <jefponte@gmail.com>
2020-11-09 16:27:46 -0300

    Algoritmo de importação sendo desenvolvido.

--- src/novissimo3s/custom/controller/Importador.php
+++ src/novissimo3s/custom/controller/Importador.php
@@ -8,12 +8,13 @@ namespace novissimo3s\custom\controller;
 use novissimo3s\model\Servico;
 use novissimo3s\model\TipoAtividade;
 use novissimo3s\model\AreaResponsavel;
+use novissimo3s\view\ServicoView;
 
 class Importador{
     
     public function importar(){
         $listaTipo = array();
-        
+        $listaServicos = array();
         if (($handle = fopen("catalogo.csv", "r")) == FALSE) {
             echo 'Falha ao tentar abrir a planilha de metas';
             return;
@@ -27,20 +28,37 @@ class Importador{
             }
             
             
-            
+            if($line == null){
+                continue;
+            }
             $linha = explode("|", $line);
             $servico = new Servico();
             $servico->setNome($linha[1]);
             $servico->setDescricao($linha[2]);
             $servico->setTempoSla(intval($linha[4]));
+            
+            
             $tipoAtividade = new TipoAtividade();
             $tipoAtividade->setNome(strtolower($linha[5]));
             $listaTipo[$tipoAtividade->getNome()] = $tipoAtividade;
             $servico->setTipoAtividade($tipoAtividade);
+            $servico->setVisao(1);
+            
             $areaResponsavel = new AreaResponsavel();
+            $areaResponsavel->setNome($linha[7]);
+            if($linha[7] == null || $linha[7] == '?'){
+                $areaResponsavel->setNome("DTI");
+                $areaResponsavel->setId(1);
+            }
+            
             
             $servico->setAreaResponsavel($areaResponsavel);
+            $listaServicos[] = $servico;
         }
+
+        $servicoview = new ServicoView();
+        $servicoview->showList($listaServicos);
+        
         
     }
     
