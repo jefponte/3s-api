0547d999dd1bf356f0ddeed3bb291769679282b5
Erivando Sena <erivandosena@gmail.com>
2023-04-20 18:08:28 -0300

    Adjust of test

---
+++ source/tests/Calculadora.php
@@ -0,0 +1,27 @@
+<?php
+// Arquivo Exemplo: Calculadora.php
+
+namespace App\Calculadora;
+
+class Calculadora
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function divide($a, $b)
+    {
+        return $a / $b;
+    }
+}
--- source/tests/Unit/ExemploTest.php
+++ source/tests/Unit/ExemploTest.php
@@ -1,43 +1,46 @@
 <?php
 // Arquivo Exemplo: ExemploTest.php
 
+namespace Tests;
+
 use PHPUnit\Framework\TestCase;
+use App\Calculadora;
 
 class ExemploTest extends TestCase
 {
-    private $calculator;
+    private $calculadora;
 
     protected function setUp(): void
     {
-        $this->calculator = new Calculator();
+        $this->calculadora = new Calculadora();
     }
 
     protected function tearDown(): void
     {
-        $this->calculator = null;
+        $this->calculadora = null;
     }
 
     public function testAddition(): void
     {
-        $result = $this->calculator->add(2, 3);
+        $result = $this->calculadora->add(2, 3);
         $this->assertEquals(5, $result);
     }
 
     public function testSubtraction(): void
     {
-        $result = $this->calculator->subtract(5, 2);
+        $result = $this->calculadora->subtract(5, 2);
         $this->assertEquals(3, $result);
     }
 
     public function testMultiplication(): void
     {
-        $result = $this->calculator->multiply(4, 6);
+        $result = $this->calculadora->multiply(4, 6);
         $this->assertEquals(24, $result);
     }
 
     public function testDivision(): void
     {
-        $result = $this->calculator->divide(10, 2);
+        $result = $this->calculadora->divide(10, 2);
         $this->assertEquals(5, $result);
     }
 }
