6727250cbbb043c9ec1d4d2412c34f8e58da7162
Erivando Sena <erivandosena@gmail.com>
2023-05-01 04:31:24 -0300

    Update Jenkinsfile

--- source/docker/database/setup-databases-env.sh
+++ source/docker/database/setup-databases-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "export PG_USER=${env.PG_USER}"
-echo "export PG_PASSWORD=${env.PG_PASSWORD}"
-echo "export PG_USER_ROOT=${env.PG_USER_ROOT}"
-echo "export PG_ROOT_PASSWORD=${env.PG_ROOT_PASSWORD}"
-echo "export PG_PASSWORD_HOMOLOGACAO=${env.PG_PASSWORD_HOMOLOGACAO}"
-echo "export DB_USER_DUMP=${env.DB_USER_DUMP}"
-echo "export DB_PASSWORD_DUMP=${env.DB_PASSWORD_DUMP}"
+echo export PG_USER=$PG_USER
+echo export PG_PASSWORD=$PG_PASSWORD
+echo export PG_USER_ROOT=$PG_USER_ROOT
+echo export PG_ROOT_PASSWORD=$PG_ROOT_PASSWORD
+echo export PG_PASSWORD_HOMOLOGACAO=$PG_PASSWORD_HOMOLOGACAO
+echo export DB_USER_DUMP=$DB_USER_DUMP
+echo export DB_PASSWORD_DUMP=$DB_PASSWORD_DUMP
