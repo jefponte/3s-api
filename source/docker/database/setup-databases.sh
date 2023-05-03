7c3310f19e8a0047fdbcb2514a915afc00a2fc70
Erivando Sena <erivandosena@gmail.com>
2023-05-02 23:22:01 -0300

    Fix bash file

--- source/docker/database/setup-databases.sh
+++ source/docker/database/setup-databases.sh
@@ -1,88 +1,63 @@
 #!/bin/bash
 
-###############################################################################
+####################################################################################################
 # 
 # Nome do arquivo: setup-databases.sh
 # Autor: Erivando Sena <erivandoramos@unilab.edu.br>
-# Data de criação: 29/04/2023
+# Data de criacao: 29/04/2023
 #
-# Descrição: Este script foi desenvolvido como parte do projeto [Stack DEVOPS DTI] da 
-#            Universidade da Integração Internacional da Lusofonia Afro-Brasileira (UNILAB).
+# Descricao: Este script foi desenvolvido como parte do projeto [Stack DEVOPS DTI] da 
+#            Universidade da Integracao Internacional da Lusofonia Afro-Brasileira (UNILAB).
 #
 # Direitos autorais (c) 2023 Erivando Sena/UNILAB.
 #
-# É concedida permissão para usar, copiar, modificar e distribuir este software apenas para 
-# uso pessoal ou em sua organização, desde que este aviso de direitos autorais apareça em 
-# todas as cópias. 
-# Este software é fornecido "como está" e sem garantias expressas ou implícitas, incluindo, 
-# mas não se limitando a, garantias implícitas de comercialização e adequação a um propósito 
-# específico. 
-# Em nenhum caso, o autor será responsável por quaisquer danos diretos, indiretos, 
-# incidentais, especiais, exemplares ou consequentes (incluindo, mas não se limitando 
-# a, aquisição de bens ou serviços substitutos, perda de uso, dados ou lucros, ou 
-# interrupção dos negócios) decorrentes do uso, incapacidade de uso ou resultados do 
+# E concedida permissao para usar, copiar, modificar e distribuir este software apenas para 
+# uso pessoal ou em sua organizacao, desde que este aviso de direitos autorais apareca em 
+# todas as copias. 
+# Este software e fornecido "como esta" e sem garantias expressas ou implicitas, incluindo, 
+# mas nao se limitando a, garantias implicitas de comercializacao e adequacao a um proposito 
+# especifico. 
+# Em nenhum caso, o autor sera responsavel por quaisquer danos diretos, indiretos, 
+# incidentais, especiais, exemplares ou consequentes (incluindo, mas nao se limitando 
+# a, aquisicao de bens ou servicos substitutos, perda de uso, dados ou lucros, ou 
+# interrupcao dos negocios) decorrentes do uso, incapacidade de uso ou resultados do 
 # uso deste software.
 #
-# Este programa é distribuído na esperança de que possa ser útil, mas SEM NENHUMA 
-# GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
-# Veja a Licença Pública Geral GNU para mais detalhes.
+# Este programa e distribuido na esperanca de que possa ser util, mas SEM NENHUMA 
+# GARANTIA; sem uma garantia implicita de ADEQUACAO a qualquer MERCADO ou APLICACAO EM PARTICULAR.
+# Veja a Licenca Publica Geral GNU para mais detalhes.
 #
-##############################################################################
+####################################################################################################
 
 # Connection options:
 #   -h, --host=HOSTNAME      host do servidor de banco de dados
 #   -p, --port=PORT          porta do servidor de banco de dados
-#   -U, --username=USERNAME  nome de usuário do banco de dados
+#   -U, --username=USERNAME  nome de usuario do banco de dados
 #   -w, --no-password        nunca solicitar senha
-#   -W, --password           forçar prompt de senha (should happen automatically)
+#   -W, --password           forcar prompt de senha (should happen automatically)
 
 set -euo pipefail
 
 connection_string_root="postgresql://$PG_USER_ROOT:$PG_ROOT_PASSWORD@$PG_HOST:$PG_PORT"
-
-# # Define as constantes
-# readonly MAX_ATTEMPTS=15
-# readonly WAIT_TIME=5
-
-# # Define a função para verificar a conexão com o PostgreSQL
-# function verifica_postgres() {
-#     local database="${1:?Database name not provided}"
-#     attempts=0
-#     until psql "$connection_string_root/$database" -c '\q'; do
-#         >&2 echo "PostgreSQL indisponível! Tentando novamente em ${WAIT_TIME} segundos..."
-#         sleep ${WAIT_TIME}
-
-#         attempts=$((attempts+1))
-#         if [ $attempts -eq $MAX_ATTEMPTS ]; then
-#             >&2 echo "Falha ao conectar ao PostgreSQL após $MAX_ATTEMPTS tentativas."
-#             exit 1
-#         fi
-#     done
-# }
-
-# set -euo pipefail
-
-# Define as constantes
 readonly MAX_ATTEMPTS=15
 readonly WAIT_TIME=5
 
-# Define a função para verificar a conexão com o PostgreSQL
+# funcoes
 function verifica_postgres() {
     local connection_string="$1"
     local attempts=0
 
-    until psql -tAc "${connection_string}/postgres" -qc '\q'; do
-        >&2 echo "PostgreSQL indisponível! Tentando novamente em ${WAIT_TIME} segundos..."
+    until psql "${connection_string}/postgres" -c '\q'; do
+        >&2 echo "PostgreSQL indisponivel! Tentando novamente em ${WAIT_TIME} segundos..."
         sleep ${WAIT_TIME}
 
         attempts=$((attempts+1))
         if [ ${attempts} -eq ${MAX_ATTEMPTS} ]; then
-            >&2 echo "Falha ao conectar ao PostgreSQL após ${MAX_ATTEMPTS} tentativas."
+            >&2 echo "Falha ao conectar ao PostgreSQL apos ${MAX_ATTEMPTS} tentativas."
             exit 1
         fi
     done
 }
-
 
 function database_exists() {
     local database_name="$1"
@@ -108,7 +83,7 @@ function create_user_admin() {
             CONNECTION LIMIT -1
             PASSWORD 'md5' || md5('$username');
 
-        COMMENT ON ROLE "$username" IS 'Usuario admin padrão.';
+        COMMENT ON ROLE "$username" IS 'Usuario admin padrao.';
 EOSQL
 }
 
@@ -125,7 +100,7 @@ function create_user_regular() {
             CONNECTION LIMIT -1
             PASSWORD 'md5' || md5('$username');
 
-        COMMENT ON ROLE "$username" IS 'Usuario regular padrão.';
+        COMMENT ON ROLE "$username" IS 'Usuario regular padrao.';
 EOSQL
 }
 
@@ -195,8 +170,10 @@ function atribui_privilegios_woner() {
 EOSQL
 }
 
+# Aguarda conexao com o PostgreSQL
 verifica_postgres "$connection_string_root"
 
+# Setup database
 if ! database_exists "$PG_DATABASE"; then
     create_database $PG_DATABASE $PG_USER
 fi
@@ -205,10 +182,11 @@ if ! database_exists "$PG_DATABASE_HOMOLOGACAO"; then
     create_database $PG_DATABASE_HOMOLOGACAO $PG_USER
 fi
 
+# Setup user
 array_users=("3s" "ocorrencias_user" "admindti")
 for i in ${!array_users[@]}; do
 
-    echo "O elemento $i é ${array_users[$i]}"
+    echo "O elemento $i e ${array_users[$i]}"
     username="${array_users[$i]}"
 
     if ! user_exists "$username"; then
@@ -227,7 +205,7 @@ done
 array_users=("cicero_robson" "luansidney" "manoeljr")
 for i in ${!array_users[@]}; do
 
-    echo "O elemento $i é ${array_users[$i]}"
+    echo "O elemento $i e ${array_users[$i]}"
     username="${array_users[$i]}"
 
     if ! user_exists "$username"; then
