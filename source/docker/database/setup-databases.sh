cd6f6e862f2cb99ea193b72e7b68bbe90f0b9059
Erivando Sena <erivandosena@gmail.com>
2023-05-05 11:03:57 -0300

    Updates sql

--- source/docker/database/setup-databases.sh
+++ source/docker/database/setup-databases.sh
@@ -97,7 +97,7 @@ function create_user_admin() {
                 CONNECTION LIMIT -1
                 PASSWORD 'md56ca6a5dafcdbc6a71988f97f4fc56fa1';
 
-            COMMENT ON ROLE "$username" IS 'Usuario admin padrao';
+            COMMENT ON ROLE $username IS 'Usuario admin padrao';
 EOSQL
     fi
 }
@@ -115,7 +115,7 @@ function create_user_regular() {
                 CONNECTION LIMIT -1
                 PASSWORD 'md56ca6a5dafcdbc6a71988f97f4fc56fa1';
 
-            COMMENT ON ROLE "$username" IS 'Usuario regular padrao';
+            COMMENT ON ROLE $username IS 'Usuario regular padrao';
 EOSQL
     fi
 }
@@ -161,11 +161,11 @@ function atribui_privilegios() {
     local database="$1"
     local user="$2"
     psql -v ON_ERROR_STOP=1 -d $connection_string_root_con <<-EOSQL
-        GRANT CONNECT ON DATABASE "$database" TO "$user";
-        GRANT USAGE, CREATE, TEMPORARY ON SCHEMA public TO "$user";
-        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT USAGE, CREATE, TEMPORARY ON TABLES TO "$user";
-        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$user";
-        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT USAGE, SELECT ON SEQUENCES TO "$user";
+        GRANT CONNECT ON DATABASE "$database" TO $user;
+        GRANT USAGE, CREATE, TEMPORARY ON SCHEMA public TO $user;
+        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT USAGE, CREATE, TEMPORARY ON TABLES TO $user;
+        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $user;
+        ALTER DEFAULT PRIVILEGES IN DATABASE "$database" GRANT USAGE, SELECT ON SEQUENCES TO $user;
 EOSQL
 }
 
@@ -173,9 +173,9 @@ function atribui_privilegios_woner() {
     local database="$1"
     local user="$2"
     psql -v ON_ERROR_STOP=1 -d $connection_string_root_con <<-EOSQL
-        ALTER DATABASE "$database" OWNER TO "$user";
-        GRANT USAGE, CREATE, TEMPORARY ON SCHEMA public TO "$user";
-        GRANT ALL PRIVILEGES ON DATABASE "$database" TO "$user";"
+        ALTER DATABASE "$database" OWNER TO $user;
+        GRANT USAGE, CREATE, TEMPORARY ON SCHEMA public TO $user;
+        GRANT ALL PRIVILEGES ON DATABASE "$database" TO $user;"
 EOSQL
 }
 
