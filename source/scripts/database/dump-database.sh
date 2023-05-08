d1dd9bddd5a1c4fa907b11b459f36805f51feb33
Erivando Sena <erivandosena@gmail.com>
2023-05-08 14:30:57 -0300

    Remove unused

--- source/scripts/database/dump-database.sh
+++ source/scripts/database/dump-database.sh
@@ -36,7 +36,6 @@
 #   -w, --no-password        nunca solicitar senha
 #   -W, --password           forcar prompt de senha (should happen automatically)
 
-# set -euo pipefail
 set +eu
 
 readonly MAX_ATTEMPTS=25
