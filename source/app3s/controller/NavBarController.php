1f1ef18a27e7a953590ea6ef2e458628772a58d5
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-05-17 22:33:26 -0300

    Convert tab to spaces

--- source/app3s/controller/NavBarController.php
+++ source/app3s/controller/NavBarController.php
@@ -40,41 +40,6 @@ class NavBarController
 ';
     if ($sessao->getNivelAcesso() == Sessao::NIVEL_ADM || $sessao->getNivelAcesso() == Sessao::NIVEL_TECNICO) {
       echo '
-                <!--
-      <li class="nav-item">
-        <a class="nav-link" href="?page=painel">Painel</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-         role="button"
-          data-toggle="dropdown"
-           aria-haspopup="true"
-            aria-expanded="false">
-          Relatórios
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Configurações
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-
-    -->
-
-
-
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Paineis
@@ -82,9 +47,6 @@ class NavBarController
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="?page=painel_kamban">Kanban</a>
                 <a class="dropdown-item" href="?page=painel_tabela">Tabela</a>
-<!--              
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a> -->
             </div>
         </li>
 
