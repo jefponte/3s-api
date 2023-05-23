1a22ddb697a297c44e331b6ae581ad6be7ac57dd
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-05-23 11:03:39 -0300

    Fix service view

--- source/app3s/controller/ServicoController.php
+++ source/app3s/controller/ServicoController.php
@@ -118,6 +118,9 @@ class ServicoController
         $list = DB::table('servico')->select('servico.id', 'servico.nome', 'servico.descricao', 'servico.tempo_sla', 'servico.visao', 'area_responsavel.nome as area_responsavel')
             ->join('area_responsavel', 'area_responsavel.id', '=', 'servico.id_area_responsavel')
             ->get();
+        foreach ($list as $key => $service) {
+            $list[$key]->visao = $this->toStringVisao($list[$key]->visao);
+        }
         echo view('admin.service.index', ['services' => $list]);
     }
 
