d55125d4c8b36ec0423a48299e20169be3150edc
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-05-27 18:51:42 -0300

    Use unilab api

--- source/app/Http/Requests/Auth/LoginRequest.php
+++ source/app/Http/Requests/Auth/LoginRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
@@ -27,7 +29,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
+            'login' => ['required', 'string'],
             'password' => ['required', 'string'],
         ];
     }
@@ -39,9 +41,49 @@ class LoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
+
+        $dataAPi = ['login' => $this->login, 'senha' => $this->password];
+
+        $response = Http::post(env('UNILAB_API_ORIGIN') . '/authenticate', $dataAPi);
+
+        $responseJ = json_decode($response->body());
+
+        $userId  = 0;
+
+        if (isset($responseJ->id)) {
+            $userId = intval($responseJ->id);
+        }
+        if ($userId === 0) {
+            throw ValidationException::withMessages([
+                'login' => trans('auth.failed'),
+            ]);
+        }
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $responseJ->access_token,
+        ];
+        $response = Http::withHeaders($headers)->get(env('UNILAB_API_ORIGIN') . '/user', $headers);
+        $responseJ2 = json_decode($response->body());
+
+        // $response = Http::withHeaders($headers)->get(env('UNILAB_API_ORIGIN') . '/bond', $headers);
+        // $responseJ3 = json_decode($response->body());
+
+
+        $user = User::firstOrNew(['id' => $userId]);
+        $user->id = $userId;
+        $user->name = $responseJ2->nome;
+        $user->email = $responseJ2->email;
+        // $user->login = $responseJ2->login;
+        // $user->division_sig = $responseJ3[0]->sigla_unidade;
+        $user->password = $this->password;
+
+        $user->save();
+
+
+        $dataAtemp = ['email' => $responseJ2->email, 'password' => $this->password];
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($dataAtemp, $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
