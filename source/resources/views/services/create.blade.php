f526e862a2cf1e9923909cfbb722a12a09e14f74
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-05-30 17:23:11 -0300

    Fix layout

--- source/resources/views/services/create.blade.php
+++ source/resources/views/services/create.blade.php
@@ -1,36 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('admin.navbar')
-    <div class="container">
-        <div class="row">
+    <div class="card">
+        <div class="card-header">Create New Service</div>
+        <div class="card-body">
+            <a href="{{ url('/services') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+            <br />
+            <br />
 
+            @if ($errors->any())
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Create New Service</div>
-                    <div class="card-body">
-                        <a href="{{ url('/services') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <br />
-                        <br />
+            {!! Form::open(['url' => '/services', 'class' => 'form-horizontal', 'files' => true]) !!}
 
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
+            @include ('services.form', ['formMode' => 'create'])
 
-                        {!! Form::open(['url' => '/services', 'class' => 'form-horizontal', 'files' => true]) !!}
-
-                        @include ('services.form', ['formMode' => 'create'])
-
-                        {!! Form::close() !!}
-
-                    </div>
-                </div>
-            </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
--- source/resources/views/services/edit.blade.php
+++ source/resources/views/services/edit.blade.php
@@ -1,41 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('admin.navbar')
-    <div class="container">
-        <div class="row">
+    <div class="card">
+        <div class="card-header">Edit Service #{{ $service->id }}</div>
+        <div class="card-body">
+            <a href="{{ url('/services') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+            <br />
+            <br />
 
+            @if ($errors->any())
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Edit Service #{{ $service->id }}</div>
-                    <div class="card-body">
-                        <a href="{{ url('/services') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <br />
-                        <br />
+            {!! Form::model($service, [
+                'method' => 'PATCH',
+                'url' => ['/services', $service->id],
+                'class' => 'form-horizontal',
+                'files' => true
+            ]) !!}
 
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
+            @include ('services.form', ['formMode' => 'edit'])
 
-                        {!! Form::model($service, [
-                            'method' => 'PATCH',
-                            'url' => ['/services', $service->id],
-                            'class' => 'form-horizontal',
-                            'files' => true
-                        ]) !!}
+            {!! Form::close() !!}
 
-                        @include ('services.form', ['formMode' => 'edit'])
-
-                        {!! Form::close() !!}
-
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 @endsection
