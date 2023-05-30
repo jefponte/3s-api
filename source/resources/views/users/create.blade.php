5b8c5c50e42c19e7f654b8b9f388235b1c6569f1
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-05-30 17:06:51 -0300

    Fix template

--- source/resources/views/users/create.blade.php
+++ source/resources/views/users/create.blade.php
@@ -2,35 +2,26 @@
 
 @section('content')
     @include('admin.navbar')
-    <div class="container">
-        <div class="row">
+    <div class="card">
+        <div class="card-header">Create New User</div>
+        <div class="card-body">
+            <a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+            <br />
+            <br />
 
+            @if ($errors->any())
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Create New User</div>
-                    <div class="card-body">
-                        <a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <br />
-                        <br />
+            {!! Form::open(['url' => '/users', 'class' => 'form-horizontal', 'files' => true]) !!}
 
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
+            @include ('users.form', ['formMode' => 'create'])
 
-                        {!! Form::open(['url' => '/users', 'class' => 'form-horizontal', 'files' => true]) !!}
-
-                        @include ('users.form', ['formMode' => 'create'])
-
-                        {!! Form::close() !!}
-
-                    </div>
-                </div>
-            </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
