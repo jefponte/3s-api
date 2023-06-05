8762ec7af20a269866a0cbb984a3dfe678263cd3
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-06-05 15:35:02 -0300

    Old structure ready to production

---
+++ source/app3s/controller/MainAjax.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace app3s\controller;
+
+
+
+class MainAjax
+{
+    
+    public function main(){
+        switch ($_GET['ajax']){
+            case 'ocorrencia':
+                $controller = new OcorrenciaController();
+                $controller->mainAjax();
+                break;
+            case 'mensagem_forum':
+                $controller = new MensagemForumController();
+                $controller->mainAjax();
+                break;
+            case 'status_ocorrencia':
+                $controller = new StatusOcorrenciaController();
+                $controller->mainAjax();
+                break;
+            case 'pedir_ajuda':
+                $controller = new OcorrenciaController();
+                $controller->ajaxPedirAjuda();
+                break;
+            case 'painel_kamban':
+                $controller = new PainelKambanController();
+                $controller->quadroKamban();
+                break;
+            case 'painel_tabela':
+                $controller = new PainelTabelaController();
+                $controller->tabelaChamados();
+                break;
+            case 'login':
+                $controller = new UsuarioController();
+                $controller->ajaxLogin();
+                break;
+            case 'mudar_nivel':
+                $controller = new UsuarioController();
+                $controller->mudarNivel();
+                break;
+            default:
+                echo ':falha';
+                break;
+        }
+        
+    }
+}
+
+?>
