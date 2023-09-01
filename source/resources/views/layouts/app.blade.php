2605b23195bd3ed2f64983dd8a03ad3e88b2edf2
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-09-01 17:14:09 -0300

    Fix layout

--- source/resources/views/layouts/app.blade.php
+++ source/resources/views/layouts/app.blade.php
@@ -4,15 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', '3s') }}</title>
-
-
-
-    <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
