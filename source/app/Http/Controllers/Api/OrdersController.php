3f8f400ce7ea443158b7153ae1eb337e78680a61
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-09-03 21:29:33 -0300

    Fix api with sanctum

---
+++ source/app/Http/Controllers/Api/OrdersController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $orders = Order::latest()->paginate(25);
+
+        return $orders;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $order = Order::create($request->all());
+
+        return response()->json($order, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $order = Order::findOrFail($id);
+
+        return $order;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $order = Order::findOrFail($id);
+        $order->update($request->all());
+
+        return response()->json($order, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Order::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
--- source/routes/api.php
+++ source/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\OrdersController;
+use App\Http\Controllers\Api\ServicesController;
 use App\Http\Controllers\Api\UsersController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
@@ -8,6 +10,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('users', [UsersController::class, 'index']);
+    Route::apiResource('users', UsersController::class);
+    Route::apiResource('services', ServicesController::class);
+    Route::apiResource('orders', OrdersController::class);
 });
 
 Route::post('/login', [AuthController::class, 'auth']);
