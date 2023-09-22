ad243b0c68d3c779ddcac3897f2c87d8bdcc21cc
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-09-22 11:37:54 -0300

    Remove modal from that js

--- source/resources/js/mudar_nivel.js
+++ source/resources/js/mudar_nivel.js
@@ -3,39 +3,17 @@ $( ".change-level" ).click(function() {
     var dados = { nivel:  $(this).attr('nivel') };
     mudarNivel(dados);
 });
-
 function mudarNivel(dados){
-
     jQuery.ajax({
         type: "POST",
         url: "?ajax=mudar_nivel",
         data: dados,
         success: function( data )
         {
-
-
-
             if(data.split(":")[1] == 'sucess'){
-
-                $("#botao-modal-resposta").click(function(){
-
-                    window.location.href = './?demanda=1';
-
-                });
-                $("#textoModalResposta").text("Nível Alterado Com Sucesso! ");
-                $("#modalResposta").modal("show");
-
+                window.location.href = './';
             }
-            else
-            {
-
-                $("#textoModalResposta").text("Falha ao tentar alterar nível! ");
-                $("#modalResposta").modal("show");
-            }
-
-
         }
     });
-
 
 }
