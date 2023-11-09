5e4307185329487bed1ebd6e5c84f8eb74aa0e0a
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-11-09 13:45:33 -0300

    Laravel clean project

--- resources/views/partials/card-info.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="p-4 mb-3 bg-light rounded">
-    <h4 class="font-italic">Sistema de Solicitação de Serviços</h4>
-    <p class="mb-0">
-        Estamos animados em anunciar o lançamento do nosso software 3s na CI/CD!
-        Com entregas mais rápidas, qualidade aprimorada e suporte eficiente.
-    </p>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app3s\controller\MainIndex;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    $main = new MainIndex();
-    $main->main();
-})->name('root');
-
-Route::post('/', function () {
-    $main = new MainIndex();
-    $main->main();
-})->name('root-post');
-
-
-
+    return view('welcome');
+});
