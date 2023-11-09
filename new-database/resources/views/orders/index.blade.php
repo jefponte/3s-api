dd4be2a22ce51210e3136fc49c258007e5ea9ed6
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-11-09 14:00:13 -0300

    Start migrate new code

--- new-database/resources/views/orders/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h3 class="pb-4 mb-4 font-italic border-bottom">
-        {{ __('Orders') }}
-    </h3>
-    Definir conteúdo da lista de orders
-
-@endsection
--- new-database/resources/views/orders/show.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    Definir conteúdo da order selecionada
-@endsection
