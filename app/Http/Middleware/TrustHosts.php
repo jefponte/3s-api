8d83a088c4f9d01e274b11cf315493aad93e8714
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-12-18 23:53:20 -0300

    Fix cors

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -1,39 +1,20 @@
 <?php
 
 namespace App\Http\Middleware;
-use Closure;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
-class TrustHosts
+class TrustHosts extends Middleware
 {
     /**
      * Get the host patterns that should be trusted.
      *
      * @return array<int, string|null>
      */
-    public function handle($request, Closure $next)
+    public function hosts(): array
     {
-        $headers = [
-            'JEF' => "Teste",
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        return [
+            '*',
         ];
-
-        if ($request->isMethod('OPTIONS'))
-        {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
-
-        return $response;
     }
 }
