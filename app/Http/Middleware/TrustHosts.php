314ca7a5d666886e8fe48d9a0779e447a2790fde
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-12-18 23:15:58 -0300

    Fix cors

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -1,20 +1,25 @@
 <?php
 
 namespace App\Http\Middleware;
+use Closure;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
-class TrustHosts extends Middleware
+class TrustHosts
 {
     /**
      * Get the host patterns that should be trusted.
      *
      * @return array<int, string|null>
      */
-    public function hosts(): array
+    public function handle($request, Closure $next)
     {
-        return [
-            "https://app3s-staging.web.app",
-        ];
+        $allowedHosts = ['https://app3s-staging.web.app/', 'https://app3s-staging.web.app'];
+
+        if (!in_array($request->getHost(), $allowedHosts)) {
+            abort(403);
+        }
+
+        return $next($request);
     }
 }
