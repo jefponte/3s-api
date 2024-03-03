923554b896fd2929489fe6e34793054ab2637b89
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-03-03 14:31:15 -0300

    Fix characters name

--- resources/views/admin/partials/card-kanban.blade.php
+++ resources/views/admin/partials/card-kanban.blade.php
@@ -1,57 +1,64 @@
 @php
-    $clientName = "JEFFERSON PONTE";
-    $bgCard = "";
-    $link = "text-light font-weight-bold p-3";
-    $texto = "text-black-50";
+    $bgCard = '';
+    $link = 'text-light font-weight-bold p-3';
+    $texto = 'text-black-50';
     $styleInline = '';
 
     switch ($order->status) {
-            case 'opened':
-                $bgCard = 'bg-warning';
-                $texto = "text-light";
-                break;
-            case 'reopened':
-                $bgCard = 'bg-warning';
-                $texto = "text-light";
-                break;
-            case 'in progress':
-                $bgCard = 'bg-info';
-                $texto = "text-light";
-                break;
-            case 'closed':
-                $bgCard = 'bg-success';
-                $texto = "text-light";
-                break;
-            case 'committed':
-                $bgCard = 'bg-success';
-                $texto = "text-light";
-                break;
-            case 'canceled':
-                $bgCard = 'bg-light';
-                $texto = "text-dark";
-                break;
-            case 'reserved':
-                $bgCard = 'bg-secondary';
-                $texto = "text-light";
-                break;
-            case 'pending customer response':
-                $styleInline = 'background-color:  #993399;';
-                $bgCard = '';
-                $texto = "text-light";
-                break;
-            case 'pending resource':
-                $styleInline = 'background-color:  #F85D10;';
-                $bgCard = '';
-                $texto = "text-light";
-                break;
-        }
+        case 'opened':
+            $bgCard = 'bg-warning';
+            $texto = 'text-light';
+            break;
+        case 'reopened':
+            $bgCard = 'bg-warning';
+            $texto = 'text-light';
+            break;
+        case 'in progress':
+            $bgCard = 'bg-info';
+            $texto = 'text-light';
+            break;
+        case 'closed':
+            $bgCard = 'bg-success';
+            $texto = 'text-light';
+            break;
+        case 'committed':
+            $bgCard = 'bg-success';
+            $texto = 'text-light';
+            break;
+        case 'canceled':
+            $bgCard = 'bg-light';
+            $texto = 'text-dark';
+            break;
+        case 'reserved':
+            $bgCard = 'bg-secondary';
+            $texto = 'text-light';
+            break;
+        case 'pending customer response':
+            $styleInline = 'background-color:  #993399;';
+            $bgCard = '';
+            $texto = 'text-light';
+            break;
+        case 'pending resource':
+            $styleInline = 'background-color:  #F85D10;';
+            $bgCard = '';
+            $texto = 'text-light';
+            break;
+    }
+    $nameParts = explode(' ', $order->provider_name);
+    $firstName = ucfirst(mb_strtolower($nameParts[0], 'UTF-8'));
+    $lastName = ucfirst(mb_strtolower(end($nameParts), 'UTF-8'));
+    $providerName = $firstName . ' ' . $lastName;
+
+    $nameParts = explode(' ', $order->customer_name);
+    $firstName = ucfirst(mb_strtolower($nameParts[0], 'UTF-8'));
+    $lastName = ucfirst(mb_strtolower(end($nameParts), 'UTF-8'));
+    $customerName = $firstName . ' ' . $lastName;
+
 @endphp
 
 
-
-
 <div class="col-sm-12 col-md-12 col-xl-6">
-    <div class="card draggable shadow-sm  {{$bgCard}}" style="height: 270px;{{$styleInline }}">
+    <div class="card draggable shadow-sm  {{ $bgCard }}" style="height: 270px;{{ $styleInline }}">
         <div class="card-body p-2">
             <div class="card-title">
 
@@ -59,16 +66,17 @@
                     #{{ $order->id }}
                 </a>
             </div>
-            <p class="{{$texto}}">
-                {{substr($order->descricao, 0, 75)}}[...]
+            <p class="{{ $texto }}">
+                {{ substr($order->descricao, 0, 75) }}[...]
             </p>
-            <small class="{{$texto}}">Demandante: {{$order->customer_name}}</small><br>
-            <small class="{{$texto}}">{{ __($order->status) }}</small>
+            <small class="{{ $texto }}">Demandante: {{ $customerName }}</small><br>
+            <small class="{{ $texto }}">{{ __($order->status) }}</small>
             @if ($order->status === 'reserved' || $order->status === 'in progress')
-                <br><small class="{{$texto}}">Responsável: {{$order->provider_name}}</small>
+                <br><small class="{{ $texto }}">Responsável: {{ $providerName }}</small>
             @endif
-            <br><small class="{{$texto}}">Aberto em {{date("d/m/Y G:i:s", strtotime($order->created_at))}} </small>
-            <br><small class="{{$texto}}">Fechado em {{date("d/m/Y G:i:s", strtotime($order->finished_at))}}</small>
+            <br><small class="{{ $texto }}">Aberto em {{ date('d/m/Y G:i:s', strtotime($order->created_at)) }}
+            </small>
+            <br><small class="{{ $texto }}">Fechado em {{ date('d/m/Y G:i:s', strtotime($order->finished_at)) }}</small>
         </div>
     </div>
 </div>
